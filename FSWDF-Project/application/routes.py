ba245897838a7f989d393201c56ae18b5838ba77
chrischism8063 <XXXxx22??>
2019-07-23 22:52:48 -0400

    start on creating data and forms or whatever

--- FSWDF-Project/application/routes.py
+++ FSWDF-Project/application/routes.py
@@ -1,6 +1,15 @@
 # main entry to the system
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, request, json, Response
+
+courseData = [
+    { "courseID": "1111", "title": "PHP 111", "description": "intro to PHP", "credits": "3", "term": "Fall, Spring" },
+    { "courseID": "1411", "title": "C++", "description": "intro to C++", "credits": "3", "term": "Summer" },
+    { "courseID": "1151", "title": "FLASK 110", "description": "intro to FLASK", "credits": "3", "term": "Spring" },
+    { "courseID": "121", "title": "C# 110", "description": "intro to C#","credits": "3", "term": "Fall, Spring" },
+    { "courseID": "1126", "title": "HTML 101","description": "intro to HTML", "credits": "3", "term": "Spring" },
+    { "courseID": "1123", "title": "NET 100", "description": "intro to NET", "credits": "3", "term": "Summer" }
+]
 
 @app.route("/")
 @app.route("/index")
@@ -14,17 +23,51 @@ def login():
     return render_template("login.html", login=True)
 
 
+
 @app.route("/courses")
-def courses():
-    courseData = [{"courseID": "1111", "title": "PHP 111", "description": "intro to PHP", "credits": "3", "term": "Fall, Spring"},
-    {"courseID": "1411", "title": "C++","description": "intro to C++", "credits": "3", "term": "Summer"},
-    {"courseID": "1151", "title": "FLASK 110","description": "intro to FLASK", "credits": "3", "term": "Spring"},
-    {"courseID": "121", "title": "C# 110", "description": "intro to C#","credits": "3", "term": "Fall, Spring"},
-    {"courseID": "1126", "title": "HTML 101","description": "intro to HTML", "credits": "3", "term": "Spring"},
-    {"courseID": "1123", "title": "NET 100","description": "intro to NET", "credits": "3", "term": "Summer"}]
-    return render_template("courses.html", courseData=courseData, courses=True)
+@app.route("/courses/<term>")
+def courses(term="Spring 2019"):   #When parameters exsist, must also add them to the render_template function call
+   
+    return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
 
 @app.route("/register")
 def register():
     return render_template("register.html", register=True)
+
+
+#add form to get items from form
+#request.args.get is to pull data from the GET method / options in the get method
+@app.route("/enrollment", methods=["GET", "POST"])      #State methods of acceptability
+def enrollment():
+    id = request.form.get['courseID']     
+    title = request.form.get['title']
+    term = request.form.get['term']
+    return render_template("enrollment.html", enrollment=True, data={"id":id, "title":title, "term":term })
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+        if(idx == None):
+            jdata = courseData
+        else:
+            jdata = courseData[int(idx)]
+        
+        return Response(json.dumps(jdata), mimetype="application/json")
+
+#create user class
+class User(db.Document):
+    user_id     =   db.IntField( unique=True )
+    first_name  =   db.StringField( max_length=50 )   
+    last_name   =   db.StringField( max_length=50 )
+    email       =   db.StringField( max_length=30 )
+    password    =   db.StringField( max_length=30 )
+
+@app.route("/user")
+def user():
+    # User(user_id=1, first_name="Christopher", last_name="Chism", email="chrischism@aol.com", password="1234ab" ).save()
+    # User(user_id=2, first_name="Bob", last_name="Stephens", email="BobStephens@aol.com", password="1256de").save()
+    users = User.objects.all()
+    return render_template("user.html", users=users)
+
--- FSWDF-Project/config.py
+++ FSWDF-Project/config.py
@@ -4,3 +4,7 @@ class Config(object):
     # keeps information secure
     SECRET_KY = os.environ.get('SECRET_KY') or "secret_string"
 
+    MONGODB_SETTINGS = {
+        'db':'UTA_Enrollment',
+        'host':'mongodb://localhost:27017/UTA_Enrollment'
+    }
